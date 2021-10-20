db45ed15124f9845b89d6dea7a945181b6638d7f
Yuri Apollov <apollovy@icloud.com>
2021-10-21 02:01:33 +0300

    Everything seems to be working.

--- iBrogueCE_iPad/BrogueCode/Rogue.h
+++ iBrogueCE_iPad/BrogueCode/Rogue.h
@@ -20,6 +20,7 @@
 //  You should have received a copy of the GNU Affero General Public License
 //  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //
+#pragma once
 
 #include <stdio.h>
 #include <stdlib.h>
