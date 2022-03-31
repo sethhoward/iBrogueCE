6e333dee649bd5eef1b7e26c495c7074f88ca287
Bob Taylor <bob@taylor.md>
2022-03-30 17:23:42 -0700

    fixed glyph texture creation. weapons and scrolls are a reasonable size now.

--- iBrogueCE_iPad/PlatformCode/RogueScene.swift
+++ iBrogueCE_iPad/PlatformCode/RogueScene.swift
@@ -21,6 +21,30 @@
 
 import SpriteKit
 
+// copied from Platform.h and adapted to Swift style constants
+// for use in defining the GlyphType enum
+let U_MIDDLE_DOT = "\u{00b7}"
+let U_FOUR_DOTS = "\u{2237}"
+let U_DIAMOND = "\u{25c7}"
+let U_FLIPPED_V = "\u{22CF}"
+let U_ARIES = "\u{2648}"        // foliage glyph
+let U_ESZETT = "\u{00df}"
+let U_ANKH = "\u{2640}"
+let U_MUSIC_NOTE = "\u{266A}"   // scroll glyph
+let U_CIRCLE = "\u{25CB}"       // ring glyph
+let U_LIGHTNING_BOLT = "\u{03DF}"   // charm glyph
+let U_FILLED_CIRCLE = "\u{25cf}"
+let U_NEUTER = "\u{26b2}"
+let U_U_ACUTE = "\u{00da}"
+let U_CURRENCY = "\u{00A4}"
+let U_UP_ARROW = "\u{2191}"     // weapon glyph
+let U_DOWN_ARROW = "\u{2193}"
+let U_LEFT_ARROW = "\u{2190}"
+let U_RIGHT_ARROW = "\u{2192}"
+let U_OMEGA = "\u{03A9}"        // omega glyph (special door)
+let U_CIRCLE_BARS = "\u{29F2}"
+let U_FILLED_CIRCLE_BARS = "\u{29F3}"
+
 extension CGSize {
     public init(rows: Int, cols: Int) {
         self = CGSize(width: rows, height: cols)
@@ -110,8 +134,7 @@ fileprivate extension RogueScene {
     
     // modify certain glyphs to overcome "Emoji effects"
     func modifyGlyphEncoding(glyph: String) -> String {
-        let aries = "\u{2648}"
-        if glyph == aries {
+        if glyph == U_ARIES {
             let bytes: [Unicode.UTF8.CodeUnit] = [0xe2,0x99,0x88,0xef,0xb8,0x8e] // extended UTF-8 coding for ARIES symbol
             return String(bytes: bytes, encoding: String.Encoding.utf8) ?? glyph
         }
@@ -157,13 +180,10 @@ fileprivate extension RogueScene {
                 return [.usesFontLeading]
             }
             
-            // TODO: fix charm
             init(glyph: String) {
                 // We want to use pretty font/centering if we can, but
                 // it makes tExT LOOk liKe THiS so we're defining characters
                 // that will be rendered at the same lineheight
-                // Note: Items "call"ed with non-standard characters aren't covered
-                // If some characters become ugly, this list can be expanded
                 switch (glyph) {
                 case "a"..."z",
                      "A"..."Z",
@@ -171,6 +191,20 @@ fileprivate extension RogueScene {
                      "!"..."?",
                      " ", "[", "/", "]", "^", "{", "|", "}", "~":
                     self = .letter
+                case U_MUSIC_NOTE :
+                    self = .scroll
+                case U_ARIES :
+                    self = .foliage
+                case U_OMEGA :
+                    self = .omega
+                case U_UP_ARROW :
+                    self = .weapon
+                case U_CIRCLE :
+                    self = .amulet
+                case U_LIGHTNING_BOLT :
+                    self = .charm
+                case U_CIRCLE :
+                    self = .ring
                 default:
                     self = .glyph
                 }
